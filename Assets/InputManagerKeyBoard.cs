401f10e6c1426bb889ddbc1371e3f78db6d0c2ee
johans2 <johans2@kth.se>
2016-05-15 13:00:41 +0200

    Added keyboard input.

---
+++ Assets/InputManagerKeyBoard.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class InputManagerKeyBoard : MonoBehaviour
+{
+    private enum MoveDiretion {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    
+    public Camera mainCamera;
+    public float moveSpeed = 10f;
+       
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveCamera(MoveDiretion.Up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveCamera(MoveDiretion.Down);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveCamera(MoveDiretion.Left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveCamera(MoveDiretion.Right);
+        }
+    }
+
+
+    void MoveCamera(MoveDiretion direction)
+    {
+        switch (direction)
+        {
+            case MoveDiretion.Up:
+                mainCamera.transform.position += new Vector3(0, moveSpeed * Time.deltaTime, 0);
+                break;
+            case MoveDiretion.Down:
+                mainCamera.transform.position += new Vector3(0, -moveSpeed * Time.deltaTime, 0);
+                break;
+            case MoveDiretion.Left:
+                mainCamera.transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+                break;
+            case MoveDiretion.Right:
+                mainCamera.transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+                break;
+            default:
+                break;
+        }
+
+    }
+    
+}
