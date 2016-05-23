17b6a17f2ab93467320a59f92bf6d202dd8ae13e
johans2 <johans2@kth.se>
2016-05-23 22:13:35 +0200

    parse_world now parses world. Added test world document.

--- WorldParser/parse_world.py
+++ WorldParser/parse_world.py
@@ -1,34 +1,44 @@
 """Parse the World xslx file and save it as a Unity readable csv."""
 from openpyxl import load_workbook
+from collections import namedtuple
 
-if __name__ == '__main__':
-    print "World parser initiated!"
-    wb = load_workbook('Chart.xlsx')
-    ws = wb["Sheet1"]
-    cell_range = 'A1:B2'
-    for row in ws.iter_rows(cell_range):
-        # print row[0].value
-        for cell in row:
-            print cell.fill.bgColor.rgb
-
+Tile = namedtuple("Tile", "TileType")
 
 def parse_color_chart(chart_worksheet):
     """Parse the color chart of the World file."""
     color_dict = {}
-
-    cell_range = 'A1:B2'
-    for row in chart_worksheet.iter_rows(cell_range):
-        print row[0].value
-        color_dict[row[0].value] = row[1].value
+    for row in chart_worksheet.rows:
+        color_dict[row[1].fill.bgColor.rgb] = row[0].value
 
     return color_dict
 
 
-def parse_world():
+def parse_world(world_worksheet, color_dict):
     """Parse the world."""
-    pass
+    world = []
+
+    for i in color_dict:
+        print i, color_dict[i]
+
+    for row in world_worksheet.rows:
+        parsed_row = []
+        for cell in row:
+            tile = Tile(TileType=color_dict[cell.fill.bgColor.rgb])
+            parsed_row.append(tile)
+        world.append(parsed_row)
+    return world
 
 
-def save_world():
+def save_world(world):
     """Save the World file."""
+    
     pass
+
+
+if __name__ == '__main__':
+    print "World parser initiated!"
+    wb = load_workbook('World.xlsx')
+    color_chart_sheet = wb["ColorChart"]
+    world_worksheet = wb["World"]
+    color_dict = parse_color_chart(color_chart_sheet)
+    world = parse_world(world_worksheet, color_dict)
