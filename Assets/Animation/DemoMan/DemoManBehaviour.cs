d7aab628e3c74574202edf9c4c9a3f4b64912117
johan_home <johansvensson@outlook.com>
2016-07-03 23:58:31 +0200

    Some more random stuff concerning animations. Hard work ahead.

---
+++ Assets/Animation/DemoMan/DemoManBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoManBehaviour : MonoBehaviour {
+
+    SpriteRenderer[] renderers;
+
+
+
+    void Awake() {
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+
+    }
+
+	void Start () {
+	
+	}
+	
+	void LateUpdate () {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+
+
+        }
+
+
+	}
+}
